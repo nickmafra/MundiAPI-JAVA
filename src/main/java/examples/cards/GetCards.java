7a74db0f6ba87e12268446dd0b4576eba3dd9d60
Amanda Maia <amandampessanha@hotmail.com>
2019-06-27 19:27:03 -0300

    :art: Cria a pasta de card e adiciona o método de GetCards.

---
+++ src/main/java/examples/cards/GetCards.java
@@ -0,0 +1,51 @@
+package examples.cards;
+
+import com.mundipagg.api.MundiAPIClient;
+import com.mundipagg.api.controllers.CustomersController;
+import com.mundipagg.api.http.client.*;
+import com.mundipagg.api.models.GetCardResponse;
+import com.mundipagg.api.models.ListCardsResponse;
+
+public class GetCards {
+    
+    public static void main(String[] args) {
+
+        String basicAuthUserName = "sk_test_q73YODBFQhyV9mod"; // The username to use with basic authentication
+        String basicAuthPassword = ""; // The password to use with basic authentication
+
+        MundiAPIClient client = new MundiAPIClient(basicAuthUserName, basicAuthPassword);
+
+        CustomersController customers_controller = new CustomersController();
+
+        String customerId = "cus_9R4KDQwhrpT0eYDV";
+
+        customers_controller.getCardsAsync(customerId, 1, 30, new APICallBack<ListCardsResponse>() {
+
+            @Override
+            public void onSuccess(HttpContext context, ListCardsResponse response) {
+                System.out.println("Cards found !");
+                System.out.println("Status response: " + context.getResponse().getStatusCode());
+
+                response.getData().isEmpty();
+
+                if(response.getData().isEmpty()) {
+                    System.out.println("My wallet is empty");
+                } else {
+                    for (GetCardResponse cardItem : response.getData()) {
+                        System.out.println("Card ID: " + cardItem.getId());
+                    }
+                    System.out.println("Cards quantity in my wallet: " + response.getPaging().getTotal());
+                }
+            }
+
+            @Override
+            public void onFailure(HttpContext context, Throwable error) {
+
+                System.out.println("Status response: " + context.getResponse().getStatusCode());
+                System.out.println(error.getMessage());
+                error.printStackTrace();
+
+            }
+        });
+    }
+}
